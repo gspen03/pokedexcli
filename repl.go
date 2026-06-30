bfc3a6114c7ac7e24862364e0b27ed61158411c0
gspen03 <244591335+gspen03@users.noreply.github.com>
2026-06-30 14:29:14 -0400

    add internals

--- repl.go
+++ repl.go
@@ -5,7 +5,15 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+
+	"github.com/bootdotdev/pokedexcli/internal/pokeapi"
 )
+
+type config struct {
+	pokeapiClient		pokeapi.Client
+	nextLocationsURL	*string
+	prevLocationsURL	*string
+}
 
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -20,7 +28,18 @@ func startRepl() {
 		}
 
 		commandName := words[0]
-		fmt.Printf("Your command was: %s\n", commandName)
+		
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback(cfg)
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
 }
 
@@ -28,4 +47,35 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name			string
+	description 	string
+	callback		func(*config) error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name: 			"exit",
+			description:	"Exit the Pokedex",
+			callback:		commandExit,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
+		},
+		"help": {
+			name:			"help",
+			description:	"Get help using the Pokedex",
+			callback:		commandHelp,
+		},
+	}
 }
